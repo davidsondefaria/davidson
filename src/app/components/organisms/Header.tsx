335a29cf5353fe4a94f27ad3fd476a51322aff9c
Davidson <davidsondefaria@gmail.com>
2023-11-27 21:42:56 -0300

    refactor: removing unecessary code

--- src/app/components/organisms/Header.tsx
+++ src/app/components/organisms/Header.tsx
@@ -1,11 +1,6 @@
 "use client";
 import React from "react";
-import {
-  AnimatePresence,
-  MotionConfig,
-  motion,
-  useScroll,
-} from "framer-motion";
+import { MotionConfig, motion } from "framer-motion";
 import NavBar from "../molecules/NavBar";
 import { cn } from "@/lib/utils";
 import { useActiveSectionContext } from "@/context";
@@ -36,15 +31,6 @@ const Header = () => {
           </div>
         )}
         <motion.div animate={{ width: headerWidth }} className="relative">
-          {/* <motion.div
-            variants={navBgVariants}
-            animate={activeSection === "Home" ? "home" : "expanded"}
-            className={
-              activeSection === "Home"
-                ? `absolute top-[${navMiddleY}px] left-[${navMiddleX}px] w-1 h-1 bg-background-100 rounded-full`
-                : `absolute w-full h-full bg-background-100 rounded-full`
-            }
-          ></motion.div> */}
           <div className={activeSection === "Home" ? "" : "px-4"}>
             <NavBar activeSection={activeSection} />
           </div>
@@ -55,62 +41,3 @@ const Header = () => {
 };
 
 export default Header;
-
-// const headerVariants = {
-//   home: { x: 0 },
-//   expanded: { x: 0 },
-// };
-
-// const Header = () => {
-//   const { activeSection, setActiveSection, setTimeOfLastClick } =
-//     useActiveSectionContext();
-
-//   const t = useScroll();
-
-//   console.log(t.scrollY, t.scrollYProgress);
-
-//   return (
-//     <motion.div
-//       variants={headerVariants}
-//       animate={activeSection === "Home" ? "home" : "expanded"}
-//       transition={{ layout: { duration: 1 } }}
-//       layout
-//       className={cn("z-50", headerClasses(activeSection))}
-//     >
-//       {activeSection === "Home" && (
-//         <motion.div
-//           layout="position"
-//           className={activeSection === "Home" ? `border` : "none"}
-//           initial={{ opacity: 0 }}
-//           animate={{ opacity: 1 }}
-//         >
-//           <h1 className="text-8xl font-bold ">Hello, I'm</h1>
-//           <h1 className="text-8xl font-bold">Davidson</h1>
-//           <p className="text-2xl">
-//             I create dynamic web solutions to deliver seamless digital
-//             experiences.
-//           </p>
-//         </motion.div>
-//       )}
-//       <motion.div layout="position" className={"relative"}>
-//         {/* <motion.div
-//           variants={navBgVariants}
-//           animate={activeSection === "Home" ? "home" : "expanded"}
-//           initi
-//           transition={{ layout: { duration: 1 } }}
-//           layout
-//           className={
-//             activeSection === "Home"
-//               ? "absolute w-0 h-0  bg-background-100 rounded-full"
-//               : "absolute w-full h-full bg-background-100 rounded-full"
-//           }
-//         ></motion.div> */}
-//         <div className={activeSection === "Home" ? "" : "px-4"}>
-//           <NavBar activeSection={activeSection} />
-//         </div>
-//       </motion.div>
-//     </motion.div>
-//   );
-// };
-
-// export default Header;
