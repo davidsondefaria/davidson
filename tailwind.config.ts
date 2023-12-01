8a280532dd4b1dabf264efb721c2fd768a8dfb37
Davidson <davidsondefaria@gmail.com>
2023-12-01 12:05:52 -0300

    fix: fiixng tailwind conflict

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -86,15 +86,11 @@ const config: Config = {
         950: "var(--accent-950)",
       },
     },
-<<<<<<< HEAD
     extend: {
       boxShadow: {
         rbaccent: "4px 4px 4px 0px var(--accent-200)",
       },
     },
-=======
-    extend: {},
->>>>>>> staging
   },
   plugins: [],
 };
