6dcfb0b505fc6fd33f09c4144f2e01cbc6c2a3ce
Davidson de Faria <davidsondefaria@gmail.com>
2023-12-13 19:42:42 -0300

    Playground (#9)
    * feat: adding animations examples
    
    * feat: adding new animations
    
    * refactor: including references
    
    * refactor: abstracting new page component

---
+++ src/app/playground/components/TapButton.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import { motion, MotionConfig } from "framer-motion";
+import "./components.css";
+
+const _TapButton = () => {
+  return (
+    <div className="w-[250px] h-[250px] border flex flex-col items-center justify-center gap-4">
+      <MotionConfig
+        transition={{
+          duration: 0.125,
+          ease: "easeInOut",
+        }}
+      >
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95, rotate: "2.5deg" }}
+          className="example-button"
+        >
+          Click me!
+        </motion.button>
+        <motion.button
+          whileHover={{ scale: 1.15 }}
+          whileTap={{ scale: 0.85, rotate: "-2.5deg" }}
+          style={{ backgroundColor: "red" }}
+          className="example-button"
+        >
+          Click me!
+        </motion.button>
+      </MotionConfig>
+    </div>
+  );
+};
+
+const TapButton = {
+  component: <_TapButton />,
+  reference: "https://www.youtube.com/watch?v=znbCa4Rr054",
+};
+
+export default TapButton;
