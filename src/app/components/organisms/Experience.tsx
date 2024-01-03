51de4d18139d954079a495f91c8c0025fcbb32cb
Davidson de Faria <davidsondefaria@gmail.com>
2024-01-03 02:14:04 -0300

    Experience (#14)
    * feat: added tailwind as hard skill
    
    * fix: fixing experience time plural
    
    * fix: fixing experience time
    
    * wip: experience page paused
    
    * feat: Added Experience Box with animated description
    
    * chore: adding prettier config for order plugin
    
    * refactor: ordering tailwind classses
    
    * feat: added experiences and finished timeline basics
    
    * feature: adding blur to navbar
    
    * feature: finishing experience timeline
    
    * fix: fixing jumping header

--- src/app/components/organisms/Experience.tsx
+++ src/app/components/organisms/Experience.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import { motion } from "framer-motion";
 import { FaTerminal, FaBook } from "react-icons/fa";
 import { useSectionInView } from "@/hooks";
 import { experienceData } from "@/lib/constants";
@@ -10,6 +11,9 @@ import ExperienceDescription from "../molecules/ExperienceDescription";
 import ExperienceContainer from "../molecules/ExperienceContainer";
 import TimelineIcons from "../atoms/TimelineIcons";
 import ExperienceTime from "../molecules/ExperienceTime";
+import useMeasure from "react-use-measure";
+import { cn } from "@/lib/utils";
+import { useActiveSectionContext } from "@/context";
 
 const icons = {
   professional: <FaTerminal className={"h-full w-full"} />,
@@ -19,45 +23,61 @@ const icons = {
 type IconKeys = keyof typeof icons;
 
 const Experience = () => {
-  const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView("Experience", 0.75);
+  const { activeSection } = useActiveSectionContext();
+  const [containerRef, { height }] = useMeasure();
   return (
-    <section
+    <motion.section
+      animate={activeSection === "Experience" ? { opacity: 1 } : { opacity: 0 }}
+      transition={{ duration: 0.3 }}
       id="experience"
       ref={ref}
-      className="mt-5 flex h-[1024px] flex-col gap-16"
+      className="py-8"
     >
-      {experienceData
-        .filter((item) => item.type !== "courses")
-        .map((item, index) => (
-          <React.Fragment key={`experience-${index}`}>
-            <ExperienceContainer boxRight={index % 2 === 0}>
-              <ExperienceTime
-                start={item.dates.start}
-                end={item.dates.end}
-                position={index % 2 === 0 ? "right" : "left"}
-              />
-              <TimelineIcons className="h-[60px]">
-                {icons[item.type as IconKeys]}
-              </TimelineIcons>
-              <ExperienceBox
-                className="w-1/2"
-                position={index % 2 === 0 ? "right" : "left"}
-              >
-                <Title>{item.title}</Title>
-                <Subtitle info={item.location}>{item.company}</Subtitle>
-                {item.descriptions.map((description, index) => (
-                  <React.Fragment key={`description-${index}`}>
-                    <ExperienceDescription
-                      item={description.item}
-                      subitem={description.subitem}
-                    />
-                  </React.Fragment>
-                ))}
-              </ExperienceBox>
-            </ExperienceContainer>
-          </React.Fragment>
-        ))}
-    </section>
+      <div ref={containerRef}>
+        <div
+          style={{ height: height }}
+          className={cn(
+            "absolute left-1/2 -z-10 -translate-x-1/2 border-[2px] border-primary-base",
+          )}
+        />
+        <div className="flex flex-col gap-16 py-8">
+          {experienceData
+            .filter((item) => item.type !== "courses")
+            .map((item, index) => (
+              <React.Fragment key={`experience-${index}`}>
+                <ExperienceContainer boxRight={index % 2 === 0}>
+                  <ExperienceTime
+                    start={item.dates.start}
+                    end={item.dates.end}
+                    position={index % 2 === 0 ? "right" : "left"}
+                  />
+                  <div>
+                    <TimelineIcons className="h-[60px] bg-none backdrop-blur-[6px]">
+                      {icons[item.type as IconKeys]}
+                    </TimelineIcons>
+                  </div>
+                  <ExperienceBox
+                    className="w-1/2"
+                    position={index % 2 === 0 ? "right" : "left"}
+                  >
+                    <Title>{item.title}</Title>
+                    <Subtitle info={item.location}>{item.company}</Subtitle>
+                    {item.descriptions.map((description, index) => (
+                      <React.Fragment key={`description-${index}`}>
+                        <ExperienceDescription
+                          item={description.item}
+                          subitem={description.subitem}
+                        />
+                      </React.Fragment>
+                    ))}
+                  </ExperienceBox>
+                </ExperienceContainer>
+              </React.Fragment>
+            ))}
+        </div>
+      </div>
+    </motion.section>
   );
 };
 
